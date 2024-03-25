9b778944e27d6a2d5aa7866739cc44b799df571c
Samuel Kamau <gachungasamuel53@gmail.com>
2024-03-25 10:03:57 +0300

    Update network_graphs.py

--- network_graphs.py
+++ network_graphs.py
@@ -6,69 +6,62 @@ G = nx.random_geometric_graph(200, 0.125)
 edge_x = []
 edge_y = []
 for edge in G.edges():
-	x0, y0 = G.nodes[edge[0]]['pos']
-	x1, y1 = G.nodes[edge[1]]['pos']
-	edge_x.append(x0)
-	edge_x.append(x1)
-	edge_x.append(None)
-	edge_y.append(y0)
-	edge_y.append(y1)
-	edge_y.append(None)
+    x0, y0 = G.nodes[edge[0]]['pos']
+    x1, y1 = G.nodes[edge[1]]['pos']
+    edge_x.extend([x0, x1, None])
+    edge_y.extend([y0, y1, None])
 
 edge_trace = go.Scatter(
-	x=edge_x, y=edge_y,
-	line=dict(width=0.5, color='#888'),
-	hoverinfo = 'none',
-	mode='lines'
-	)
+    x=edge_x, y=edge_y,
+    line=dict(width=0.5, color='#888'),
+    hoverinfo='none',
+    mode='lines')
 
 node_x = []
 node_y = []
 
 for node in G.nodes():
-	x, y = G.nodes[node]['pos']
-	node_x.append(x)
-	node_y.append(y)
+    x, y = G.nodes[node]['pos']
+    node_x.append(x)
+    node_y.append(y)
 
-node_trace = go.scatter(x=node_x, y=node_y, mode='markers', hoverinfo='text', 
-		marker=dict(
-			showscale=True,
-			colorscale='YlGnBu',
-			reversescale=True,
-			color=[],
-			size=10,
-			colorbar=dict(
-				thickness=15,
-				title='Node Connections',
-				xanchor='left',
-				titleside='right'),
-			line_width=2))
+node_trace = go.Scatter(
+    x=node_x, y=node_y, mode='markers', hoverinfo='text',
+    marker=dict(
+        showscale=True,
+        colorscale='YlGnBu',
+        reversescale=True,
+        color=[],
+        size=10,
+        colorbar=dict(
+            thickness=15,
+            title='Node Connections',
+            xanchor='left',
+            titleside='right'),
+        line_width=2))
 
-
-node_adjancencies = []
+node_adjacencies = []
 node_text = []
-for node, adjancencies in enumerate(G.adjancecy()):
-	node_adjancencies.append(len(adjancencies[1]))
-	node_text.append('# of connections: '+str(len(adjancencies[1])))
+for node, adjacencies in enumerate(G.adjacency()):
+    node_adjacencies.append(len(adjacencies[1]))
+    node_text.append('# of connections: ' + str(len(adjacencies[1])))
 
-node_trace.marker.color = node_adjancencies
+node_trace.marker.color = node_adjacencies
 node_trace.text = node_text
 
-
-
 fig = go.Figure(data=[edge_trace, node_trace],
-	layout=go.layout(
-		title='<br>Network graph made with Python',
-		titlefont_size=16,
-		showlegend=False,
-		hovermode='closest',
-		margin=dict(b=20,l=5,r=5.t=40),
-		annotations=[ dict(
-			text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
-			showarrow=False,
-			xref='paper', yref='paper',
-			x=0.005, y=-0.002 ) ],
-		xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-		yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
-	)
+                layout=go.Layout(
+                    title='<br>Network graph made with Python',
+                    titlefont_size=16,
+                    showlegend=False,
+                    hovermode='closest',
+                    margin=dict(b=20, l=5, r=5, t=40),
+                    annotations=[dict(
+                        text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                        showarrow=False,
+                        xref='paper', yref='paper',
+                        x=0.005, y=-0.002)],
+                    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
+                ))
 fig.show()
